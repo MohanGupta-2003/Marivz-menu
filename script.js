c912c67870b9cfc95f26d35f87503303249aff62
MohanGupta-2003 <guptamohan0502@gmail.com>
2024-11-29 15:51:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('demo-button').addEventListener('click', () => {
+  alert('AR Demo Coming Soon! Stay Tuned!');
+});
+
+document.getElementById('contact-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  alert('Thank you for contacting us! We will get back to you soon.');
+});
